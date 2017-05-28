4aeb96112a23db24f1d41af6f81cea3fce070be5
Alejandro Ferrera <aleferrera@hotmail.com>
2017-05-27 20:47:50 -0600

    Add XML Serializer Nugget

--- Artorias/Program.cs
+++ Artorias/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using LexerAnalyser;
 using SyntaxAnalyser;
 using SyntaxAnalyser.Parser;
+using System.Xml.Serialization;
+using SyntaxAnalyser.Nodes;
 
 namespace Artorias
 {
@@ -13,7 +16,9 @@ namespace Artorias
             var stream = new FileInputStream(args[0]);
             var lexer = new Lexer(stream);
             var parser = new Parser(lexer);
-            parser.Parse();
+            var code = parser.Parse();
+            XmlSerializer serializer = new XmlSerializer(typeof(NamesapceDeclaration));
+            serializer.Serialize(File.Create("C:\\Users\\alefe\\Documents\\Code\\C#\\Compiler\\serialize.xml"), code.GlobalNamespace);
             Console.WriteLine("SUCCESS");
         }
     }
--- SyntaxAnalyser/Nodes/UsingNamespaceDeclaration.cs
+++ SyntaxAnalyser/Nodes/UsingNamespaceDeclaration.cs
@@ -8,5 +8,10 @@
         {
             Identifier = identifier;
         }
+
+        public UsingNamespaceDeclaration()
+        {
+            
+        }
     }
 }
